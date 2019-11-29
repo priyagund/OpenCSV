a170d14d8a3c596c1ef17d7be0271d6487fb122a
priyagund <priyagund18@gmail.com>
2019-11-29 12:16:29 +0530

    ADD:reading all csv containts into memory

--- src/main/java/OpenCSVReadAndParseToBean.java
+++ src/main/java/OpenCSVReadAndParseToBean.java
@@ -6,6 +6,7 @@ import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.List;
 
 public class OpenCSVReadAndParseToBean
 {
@@ -22,10 +23,19 @@ public class OpenCSVReadAndParseToBean
                     .withIgnoreLeadingWhiteSpace(true)
                     .build();
 
-            Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
+           /* Iterator<CSVUser> csvUserIterator = csvToBean.iterator();
 
             while (csvUserIterator.hasNext()) {
                 CSVUser csvUser = csvUserIterator.next();
+                System.out.println("Name : " + csvUser.getName());
+                System.out.println("Email : " + csvUser.getEmail());
+                System.out.println("PhoneNo : " + csvUser.getPhoneNo());
+                System.out.println("Country : " + csvUser.getCountry());
+                System.out.println("==========================");
+            }*/
+            List<CSVUser> csvUsers = csvToBean.parse();
+
+            for(CSVUser csvUser: csvUsers) {
                 System.out.println("Name : " + csvUser.getName());
                 System.out.println("Email : " + csvUser.getEmail());
                 System.out.println("PhoneNo : " + csvUser.getPhoneNo());
